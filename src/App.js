35aba1eb14f2b1b66e8dbcdf70e6276438f39a7d
Travis <travis@nervos.org>
2023-04-24 11:58:40 -0400

    Fixed Waiting for Mint bug

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ function App() {
   const [message, setMessage] = useState("")
   const [isWaiting, setIsWaiting] = useState(false)
 
+  const [isMintEnabled, setIsMintEnabled] = useState(false)
+
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     setProvider(provider)
@@ -123,6 +126,15 @@ function App() {
     loadBlockchainData()
   }, [])
 
+  useEffect(() => {
+    if (nft !== null) {
+      setIsMintEnabled(true);
+    } else {
+      setIsMintEnabled(false);
+    }
+  }, [nft])
+
+
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
@@ -131,7 +143,8 @@ function App() {
         <form onSubmit={submitHandler}>
           <input type="text" placeholder="Create a name..." onChange={(e) => { setName(e.target.value) }} />
           <input type="text" placeholder="Create a description..." onChange={(e) => setDescription(e.target.value)} />
-          <input type="submit" value="Create & Mint" />
+          <input type="submit" value="Create & Mint" disabled={!isMintEnabled} />
+
         </form>
 
         <div className="image">
